d9733faf038a62e4c8eed0be57c4de2d99f4baae
Saeedul <karimsaeedul476@gmail.com>
2024-07-05 04:45:13 +0600

    added footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 $(function (){
+    "use strict"
+    $(window).on('scroll', function (event){
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".navbar-area").removeClass("sticky");
+        } else {
+            $(".navbar-area").addClass("sticky")
+        }
+    });
     //wow js
     new WOW().init();
 })
